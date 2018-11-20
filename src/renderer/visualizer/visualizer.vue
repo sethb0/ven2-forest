bb886a3f286826aa62b6f2c2f1a716026099b450
Seth L. Blumberg <sethb@pobox.com>
2018-11-20 18:11:53 -0500

    change visualizer to use CSS variables for styling

--- src/renderer/visualizer/visualizer.vue
+++ src/renderer/visualizer/visualizer.vue
@@ -1,14 +1,119 @@
 <template>
-  <div id="visualizer"></div>
+  <div id="visualizer" :class="visClass"></div>
 </template>
 
 <style>
-#visualizer { background-color: ghostwhite; overflow: hidden; }
+:root {
+  --visualizer-background: ghostwhite;
+  --visualizer-edge: black;
+  --visualizer-edge-width: 1px;
+
+  --abyssal-charm: darkgray;
+  --abyssal-cluster: gainsboro;
+  --alchemical-charm: darkorange;
+  --alchemical-cluster: navajowhite;
+  --dragon-blooded-charm: indianred;
+  --dragon-blooded-cluster: rosybrown;
+  --infernal-charm: lightgreen;
+  --infernal-cluster: darkseagreen;
+  --jadeborn-charm: peru;
+  --jadeborn-cluster: burlywood;
+  --lunar-charm: lightskyblue;
+  --lunar-cluster: dodgerblue;
+  --raksha-charm: forestgreen;
+  --raksha-cluster: yellowgreen;
+  --sidereal-charm: plum;
+  --sidereal-cluster: mediumpurple;
+  --solar-charm: gold;
+  --solar-cluster: goldenrod;
+  --tma-charm: indianred;
+  --tma-cluster: rosybrown;
+  --cma-charm: lightsteelblue;
+  --cma-cluster: thistle;
+  --sma-charm: plum;
+  --sma-cluster: mediumpurple;
+}
+
+#visualizer {
+  overflow: hidden;
+  background-color: var(--visualizer-background);
+  stroke-linecap: round;
+}
+
+#visualizer.abyssal {
+  --visualizer-charm: var(--abyssal-charm);
+  --visualizer-cluster: var(--abyssal-cluster);
+}
+#visualizer.alchemical {
+  --visualizer-charm: var(--alchemical-charm);
+  --visualizer-cluster: var(--alchemical-cluster);
+}
+#visualizer.dragon-blooded {
+  --visualizer-charm: var(--dragon-blooded-charm);
+  --visualizer-cluster: var(--dragon-blooded-cluster);
+}
+#visualizer.infernal {
+  --visualizer-charm: var(--infernal-charm);
+  --visualizer-cluster: var(--infernal-cluster);
+}
+#visualizer.jadeborn {
+  --visualizer-charm: var(--jadeborn-charm);
+  --visualizer-cluster: var(--jadeborn-cluster);
+}
+#visualizer.lunar {
+  --visualizer-charm: var(--lunar-charm);
+  --visualizer-cluster: var(--lunar-cluster);
+}
+#visualizer.raksha {
+  --visualizer-charm: var(--raksha-charm);
+  --visualizer-cluster: var(--raksha-cluster);
+}
+#visualizer.sidereal {
+  --visualizer-charm: var(--sidereal-charm);
+  --visualizer-cluster: var(--sidereal-cluster);
+}
+#visualizer.solar {
+  --visualizer-charm: var(--solar-charm);
+  --visualizer-cluster: var(--solar-cluster);
+}
+#visualizer.terrestrial-martial-arts {
+  --visualizer-charm: var(--tma-charm);
+  --visualizer-cluster: var(--tma-cluster);
+}
+#visualizer.celestial-martial-arts {
+  --visualizer-charm: var(--cma-charm);
+  --visualizer-cluster: var(--cma-cluster);
+}
+#visualizer.sidereal-martial-arts {
+  --visualizer-charm: var(--sma-charm);
+  --visualizer-cluster: var(--sma-cluster);
+}
+
+#visualizer text {
+  fill: var(--visualizer-edge);
+}
+#visualizer .edge *|path, #visualizer *|polygon, #visualizer *|ellipse {
+  stroke: var(--visualizer-edge);
+}
+#visualizer .edge *|path, #visualizer .edge *|polygon {
+  stroke-width: var(--visualizer-edge-width);
+}
+#visualizer .edge *|polygon {
+  fill: var(--visualizer-edge);
+}
+#visualizer .node *|polygon, #visualizer .node *|ellipse {
+  fill: var(--visualizer-charm);
+}
+#visualizer .cluster *|polygon {
+  fill: var(--visualizer-cluster);
+}
 </style>
 
 <script>
 import { mapState } from 'vuex/dist/vuex.esm';
 import svgPanZoom from 'svg-pan-zoom';
+
+import { toKebab } from '../../common/util';
 
 const MIN_ZOOM = 0.02;
 
@@ -16,7 +121,12 @@ let postprocessedElement;
 let panZoom;
 
 export default {
-  computed: mapState(['svgElement']),
+  data () {
+    return { visClass: '' };
+  },
+  computed: {
+    ...mapState(['activeType', 'svgElement']),
+  },
   methods: {
     onClick (evt) {
       let element = evt.target;
@@ -46,9 +156,11 @@ export default {
           center: false,
         });
         panZoom.zoomAtPoint(MIN_ZOOM, { x: 0, y: 0 });
+        this.visClass = toKebab(this.activeType);
       } else {
         postprocessedElement = null;
         panZoom = null;
+        this.visClass = '';
       }
     },
   },
@@ -58,6 +170,16 @@ function postprocess (svg, handler) {
   svg.getElementById('a_graph0').remove();
   for (const el of svg.querySelectorAll('title')) {
     el.remove();
+  }
+  for (const el of svg.querySelectorAll('[fill]')) {
+    if (el.getAttribute('fill') !== 'none') {
+      el.removeAttribute('fill');
+    }
+  }
+  for (const el of svg.querySelectorAll('[stroke]')) {
+    if (el.getAttribute('stroke') !== 'none') {
+      el.removeAttribute('stroke');
+    }
   }
   for (const el of svg.querySelectorAll('g.node a, g.cluster a')) {
     el.onclick = handler;
